2c68a6532016805493cdc318c401f828d4274952
Devi Priya Karuppiah <priyamadurai2022@gmail.com>
2024-10-22 11:54:28 +0530

    steps 3 and 4 of transform and load complete

--- dags/etl.py
+++ dags/etl.py
@@ -50,13 +50,47 @@ with DAG(
         response_filter=lambda response:response.json(), ## Convert response to json
     )
 
-    
 
     ## Step 3: Transform the data(Pick the information that i need to save)
-   
+    @task
+    def transform_apod_data(response):
+        apod_data={
+            'title': response.get('title', ''),
+            'explanation': response.get('explanation', ''),
+            'url': response.get('url', ''),
+            'date': response.get('date', ''),
+            'media_type': response.get('media_type', '')
+
+        }
+        return apod_data    
 
 
     ## step 4:  Load the data into Postgres SQL
+    @task
+    def load_data_to_postgres(apod_data):
+        ## Initialize the PostgresHook
+        postgres_hook=PostgresHook(postgres_conn_id='my_postgres_connection')
+
+        ## Define the SQL Insert Query
+
+        insert_query = """
+        INSERT INTO apod_data (title, explanation, url, date, media_type)
+        VALUES (%s, %s, %s, %s, %s);
+        """
+
+        ## Execute the SQL Query
+
+        postgres_hook.run(insert_query,parameters=(
+            apod_data['title'],
+            apod_data['explanation'],
+            apod_data['url'],
+            apod_data['date'],
+            apod_data['media_type']
+
+
+        ))
+
+
 
 
     ## step 5: Verify the data DBViewer
