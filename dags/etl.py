510615622300683304e06f775ae809b6d522303d
Devi Priya Karuppiah <priyamadurai2022@gmail.com>
2024-10-22 11:46:12 +0530

    step 2 complete - extract nasa API data

--- dags/etl.py
+++ dags/etl.py
@@ -40,7 +40,7 @@ with DAG(
 
 
     ## Step 2: Extract the NASA API Data(APOD)-Astronomy Picture of the Day[Extract pipeline]
-    ## https://api.nasa.gov/planetary/apod?api_key=7BbRvxo8uuzas9U3ho1RwHQQCkZIZtJojRIr293q
+    ## https://api.nasa.gov/planetary/apod?api_key=your_unique_key
     extract_apod=SimpleHttpOperator(
         task_id='extract_apod',
         http_conn_id='nasa_api',  ## Connection ID Defined In Airflow For NASA API
@@ -53,54 +53,13 @@ with DAG(
     
 
     ## Step 3: Transform the data(Pick the information that i need to save)
-    @task
-    def transform_apod_data(response):
-        apod_data={
-            'title': response.get('title', ''),
-            'explanation': response.get('explanation', ''),
-            'url': response.get('url', ''),
-            'date': response.get('date', ''),
-            'media_type': response.get('media_type', '')
-
-        }
-        return apod_data
+   
 
 
     ## step 4:  Load the data into Postgres SQL
-    @task
-    def load_data_to_postgres(apod_data):
-        ## Initialize the PostgresHook
-        postgres_hook=PostgresHook(postgres_conn_id='my_postgres_connection')
-
-        ## Define the SQL Insert Query
-
-        insert_query = """
-        INSERT INTO apod_data (title, explanation, url, date, media_type)
-        VALUES (%s, %s, %s, %s, %s);
-        """
-
-        ## Execute the SQL Query
-
-        postgres_hook.run(insert_query,parameters=(
-            apod_data['title'],
-            apod_data['explanation'],
-            apod_data['url'],
-            apod_data['date'],
-            apod_data['media_type']
-
-
-        ))
-
 
 
     ## step 5: Verify the data DBViewer
 
 
     ## step 6: Define the task dependencies
-    ## Extract
-    create_table() >> extract_apod  ## Ensure the table is create befor extraction
-    api_response=extract_apod.output
-    ## Transform
-    transformed_data=transform_apod_data(api_response)
-    ## Load
-    load_data_to_postgres(transformed_data)
